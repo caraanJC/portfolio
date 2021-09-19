20264ecf1e50de4542ee595bf1a25569c1631a18
caraanJC <caraanjohncarlo@gmail.com>
2021-09-19 22:11:55 +0800

    feat: remove wrong folder

--- full stack web application/client/src/components/Popup.js
+++
@@ -1,9 +0,0 @@
-import '../styles/Popup.css';
-
-const Popup = ({ message, type }) => {
-  return (
-    <div className={`popup ${type ? 'popup-' + type : ''}`}>{message}</div>
-  );
-};
-
-export default Popup;
