baae0b452d1ce3a7713cca516bf2e8575f3953ea
John Carlo Caraan <caraanjohncarlo@gmail.com>
2024-10-11 09:42:17 +0800

    chore: update baseURL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
-import { name } from '@/../package.json'
+// import { name } from '@/../package.json'
 
-export const baseURL = name ? `/${name}` : ''
+// export const baseURL = name ? `/${name}` : ''
+export const baseURL = ''
 export const secondaryColor = '#da4ea2'
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { name} from './package.json'
+// import { name} from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: name,
+  // base: name,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, './src')
