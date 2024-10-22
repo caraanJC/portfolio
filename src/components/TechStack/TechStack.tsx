f3228dc2c4951db71bc8a31ebcf7b11156e2c06b
John Carlo Caraan <caraanjohncarlo@gmail.com>
2024-10-22 16:00:12 +0800

    feat: add tech stacks

--- src/components/TechStack/TechStack.tsx
+++ src/components/TechStack/TechStack.tsx
@@ -6,18 +6,16 @@ const TechStack = () => {
     <div id="techStack" className="techStack">
       <h2 className="subheading">My Tech Stack</h2>
       <div className="techStack-gallery">
-        <div>
-          <img className="techStack-gallery-img" src="/images/html5.svg" loading="lazy" />
-          <img className="techStack-gallery-img" src="/images/css3.svg" loading="lazy" />
-          <img className="techStack-gallery-img" src="/images/js.svg" loading="lazy" />
-          <img className="techStack-gallery-img" src="/images/react.svg" loading="lazy" />
-        </div>
-        <div>
-          <img className="techStack-gallery-img" src="/images/git.svg" loading="lazy" />
-          <img className="techStack-gallery-img" src="/images/vscode.svg" loading="lazy" />
-          <FaGithub className="techStack-gallery-img" />
-          <img className="techStack-gallery-img" src="/images/mongodb.svg" loading="lazy" />
-        </div>
+        <img className="techStack-gallery-img" src="/images/html5.svg" loading="lazy" />
+        <img className="techStack-gallery-img" src="/images/css3.svg" loading="lazy" />
+        <img className="techStack-gallery-img" src="/images/js.svg" loading="lazy" />
+        <img className="techStack-gallery-img" src="/images/react.svg" loading="lazy" />
+        <img className="techStack-gallery-img" src="/images/git.svg" loading="lazy" />
+        <img className="techStack-gallery-img" src="/images/vscode.svg" loading="lazy" />
+        <FaGithub className="techStack-gallery-img" />
+        <img className="techStack-gallery-img" src="/images/mongodb.svg" loading="lazy" />
+        <img className="techStack-gallery-img" src="/images/ts.svg" loading="lazy" />
+        <img className="techStack-gallery-img" src="/images/flutter.svg" loading="lazy" />
       </div>
     </div>
   )
